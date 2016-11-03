df7eaaa686361731ec8d2759b6d810a2748d875c
H4ndm1x3r <nlloyd0001@gmail.com>
2016-11-03 17:58:30 +0100

    Code Improvements
    Conceptual randomized blinking.
    Possibly very fast at the moment.

--- Will_o.ino
+++ Will_o.ino
@@ -1,33 +1,50 @@
-int ledPin[] = {}; // Add PWM pins
-int dB = 0; // Add microphone value
-// int LED_group1 = 11; // Add pin
-// int LED_group2 = 10; // Add pin
-// int LED_group3 = 9; // Add pin
+// Software for the Will-o-Wisp concept.
+// Created in collaboration with Digital Experience & Aesthetics.
+// Code by Nicholas Krasuski, BSc Digital Media and Design
 
-int bright = 0; // Adjusted later on
-int fade = 0; // Adjusted later on
+// Values used for output/LED addressing
+int ledPin[15] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 46, 45, 44}; // Add PWM pins
+int ledState[15] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW}; // Reserved led state
+int ledCount = 15; // The amount of LEDs
+int ledOn = false;
+int bright = 0; // Reserved brightness value
+int fade = 0; // Reserved fade value
+
+// Microphone/input values
+int micPin = A0; // Pin used for analog microphone input
+int micThresh = 960; // Microphone threshold level
+int micValue = 0;
 
 void setup() {
-  pinMode(LED_group1, OUTPUT);
-  pinMode(LED_group2, OUTPUT);
-//  pinMode(LED_group3, OUTPUT);
+  // Setup for led pins
+  for (int i=0; i < ledCount; i++) {
+    pinMode(ledPin[i], OUTPUT); // Sets each pin in array to output mode
+  }
+  // Setup for microphone
+  pinMode(micPin, INPUT);
+  Serial.begin(9600);  
 }
 
 void loop() {
+  micValue = analogRead(micPin);
 // Aggressive blinking
-  if (dB < 30 && dB > 25) {
-    digitalWrite(ledPin[x],HIGH); // Add array numbers when known
-    // Continue digitalWrite for Applicable amount
+  if (micValue  < micThresh) {
+    for (int p = 0; p < ledCount; p++) { // For loop to randomize the LED thingies
+      int pinNum = random(0,15);
+      ledState[pinNum] = !ledState[pinNum]
+      digitalWrite(ledPin[pinNum], ledState[pinNum]);
+    }
+    ledOn = !ledOn;
+    // Timeout to avoid high speeds
   }
-  //digitalWrite(LED_group1, HIGH);
-  //digitalWrite(LED_group2, LOW);
-  delay(200);
-  //digitalWrite(LED_group1, LOW);
-  //digitalWrite(LED_group2, HIGH);
-  delay(200);
   
 // Slow pulse/breathing
-  if (dB < 25) {
+  if (micValue > micThresh) {
+    if (ledOn == true) {
+      for (int o = 0; 0 < ledCount; o++) { //
+      digitalWrite(ledPin[o], LOW);
+      }
+    } 
     // Group LEDs possibly
     analogWrite(ledPin[x], brightness); // Add array numbers when known
     // Continue analogWrite for applicable amount
