6a7b7ea480e2cdc14e06e9f8214de969142b8916
H4ndm1x3r <nlloyd0001@gmail.com>
2016-11-06 11:35:33 +0100

    Added hide-mode
    > Checked for syntax errors
    > Added a hide mode after 20 seconds in the 'aggressive blinking'
    program.
    > Added timing in the breathing, to slow down sequence a bit.
    > Lets do this thing.

--- Will_o.ino
+++ Will_o.ino
@@ -32,7 +32,7 @@ void loop() {
     for (int p = 0; p < 50000; p++) { // For loop to randomize the LED thingies
       int pinNum = random(0,15);
       if (passedTime == passedThresh) { // Check if threshold milliseconds has been reached
-        if (ledState[pinNum] == HIGH) { // detect prior state in order to reverse
+        if (ledState[pinNum] == HIGH) { // Detect prior state in order to reverse
           ledState[pinNum] = LOW;
         }
         else {
@@ -41,47 +41,52 @@ void loop() {
         digitalWrite(ledPin[pinNum], ledState[pinNum]); // Write decided state to selected pin
         passedThresh = passedThresh + 200; // Increase the threshold by 200 milliseconds
       }
-      
     }
     ledOn = true; // Used to tell whether program has been run
-    Serial.printIn("PERSON DETECTED") // Serial confirmation for debugging
-    /*
-     * ADD A 'HIDE' MODE AFTER CERTAIN AMOUNT OF BLINKING
-     *   > DONE WITH COUNTER INSIDE FOR-LOOP
-     *   > RESET COUNTER EACH TIME
-     */
+    Serial.print("PERSON DETECTED!"); // Serial confirmation for debugging
+    if (passedTime == 20000) { // 'hide' mode
+      for (int l = 0; l < ledCount; l++) {
+        digitalWrite(ledPin[l], LOW); // turn off all LED's
+      }
+      Serial.print("OH NO I'M SCARED!"); // Serial confirmation for debugging
+    }
   }
   
 // Slow pulse/breathing
   if (micValue > micThresh) {
+    int passedTime = millis();
+    int passedThresh = 300;
     if (ledOn == true) { // Detect if Aggressive Blinking program has run
       for (int o = 0; 0 < ledCount; o++) { // Turn off all LEDs for fresh start
       digitalWrite(ledPin[o], LOW);
       ledFade[o] = 0;
       }
     }
-    for (int q = 0; p < 100000; q++) { // For-loop to increase/decrease fade amount on LEDs
+    for (int q = 0; q < 100000; q++) { // For-loop to increase/decrease fade amount on LEDs
       int pinNum = random(0,15); // Choose a random pin
-      if (ledFade[pinNum] == 0) { // if-statement to increase brightness level
-        for (int fade = 0; fade <= 255; fade += 5) {
-          analogWrite(ledPin[pinNum],fade);
-          if (fade == 255) {
-            ledFade[pinNum] = fade;
-          }
+      if (passedTime == passedThresh) {
+        if (ledFade[pinNum] == 0) { // if-statement to increase brightness level
+          for (int fade = 0; fade <= 255; fade += 5) {
+            analogWrite(ledPin[pinNum],fade);
+            if (fade == 255) {
+              ledFade[pinNum] = fade;
+            }
           delay(30); // Delay to visualize the fade effect
-        }
-      }
-      if (ledFade[pinNum] > 0) { // if-statement to decrease brightness level
-        for (int fade = 255; fade >= 0; -= 5) {
-          analogWrite(ledPin[pinNum], fade);
-          if (fade == 0) {
-            ledFade[pinNum] = fade;
           }
-          delay(30); // Delay to visualize the fade effect
         }
+        if (ledFade[pinNum] > 0) { // if-statement to decrease brightness level
+          for (int fade = 255; fade >= 0; fade -= 5) {
+            analogWrite(ledPin[pinNum], fade);
+            if (fade == 0) {
+              ledFade[pinNum] = fade;
+            }
+            delay(30); // Delay to visualize the fade effect
+          }
+        }
+        passedThresh = passedThresh + 300;  
       }
     }
+    Serial.print("BREATHING");
   }
-// A 20 or so second sleep while selected program runs
 }
 
